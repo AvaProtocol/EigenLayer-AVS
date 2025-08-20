96f4f2bf5f2dcaa828fffd66498410707ea8198b
Wei Lin <wei@avaprotocol.org>
2025-08-20 15:41:02 -0700

    Remove redundant files

--- fix_trigger_reason.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Fix all TriggerReason structures in engine_test.go
-cd /Users/mikasa/Code/EigenLayer-AVS
-
-# Replace the Data field pattern with the correct structure
-perl -i -pe 's/Data: &avsproto\.TriggerReason_Block\{\s*Block: &avsproto\.BlockTriggerData\{\s*BlockNumber: uint64\((\d+)\),\s*\},\s*\},/BlockNumber: uint64($1),\n\t\t\tType:        avsproto.TriggerReason_Block,/g' core/taskengine/engine_test.go
-
-echo "Fixed TriggerReason structures in engine_test.go" 
