294f4ecf362783cea076c97be5fca9ffac8d1fcf
Will Zimmerman <will@avaprotocol.org>
2025-12-17 14:52:09 -0800

    fix: remove redundant log.WriteString calls in ContractRead error handling
    The error messages are already captured by finalizeStep via err.Error(),
    so writing them to the log buffer before finalizeStep is redundant.

--- core/taskengine/vm_runner_contract_read.go
+++ core/taskengine/vm_runner_contract_read.go
@@ -404,7 +404,6 @@ func (r *ContractReadProcessor) Execute(stepID string, node *avsproto.ContractRe
 
 	// Get configuration from node config
 	if err = validateNodeConfig(node.Config, "ContractReadNode"); err != nil {
-		log.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
 		finalizeStep(s, false, err, err.Error(), log.String())
 		return s, err
 	}
@@ -413,13 +412,11 @@ func (r *ContractReadProcessor) Execute(stepID string, node *avsproto.ContractRe
 	// Note: ABI is handled directly from protobuf Values using optimized parsing
 	if config.ContractAddress == "" {
 		err = NewMissingRequiredFieldError("contractAddress")
-		log.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
 		finalizeStep(s, false, err, err.Error(), log.String())
 		return s, err
 	}
 	if len(config.ContractAbi) == 0 {
 		err = NewMissingRequiredFieldError("contractAbi")
-		log.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
 		finalizeStep(s, false, err, err.Error(), log.String())
 		return s, err
 	}
@@ -430,7 +427,6 @@ func (r *ContractReadProcessor) Execute(stepID string, node *avsproto.ContractRe
 	// Validate contract address after template resolution
 	if !common.IsHexAddress(contractAddress) {
 		err = NewInvalidAddressError(contractAddress)
-		log.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
 		finalizeStep(s, false, err, err.Error(), log.String())
 		return s, err
 	}
@@ -438,7 +434,6 @@ func (r *ContractReadProcessor) Execute(stepID string, node *avsproto.ContractRe
 
 	if len(config.MethodCalls) == 0 {
 		err = NewMissingRequiredFieldError("methodCalls")
-		log.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
 		finalizeStep(s, false, err, err.Error(), log.String())
 		return s, err
 	}
@@ -450,13 +445,11 @@ func (r *ContractReadProcessor) Execute(stepID string, node *avsproto.ContractRe
 			parsedABI = optimizedParsedABI
 		} else {
 			err = fmt.Errorf("failed to parse ABI: %v", parseErr)
-			log.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
 			finalizeStep(s, false, err, err.Error(), log.String())
 			return s, err
 		}
 	} else {
 		err = NewMissingRequiredFieldError("contractAbi")
-		log.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
 		finalizeStep(s, false, err, err.Error(), log.String())
 		return s, err
 	}
